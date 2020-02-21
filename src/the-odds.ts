5c932bd69c8788988fc1dc34d3e0ed30fa196e95
Hugh Rawlinson <hughr2005@gmail.com>
2020-02-21 14:57:47 -0500

    refactor: add Pollard's Rho algo

--- src/the-odds.ts
+++ src/the-odds.ts
@@ -15,6 +15,28 @@ export function gcd(x: number, y: number) {
   return a;
 }
 
+// This is for integer factorization
+// https://en.wikipedia.org/wiki/Pollard%27s_rho_algorithm
+export function pollardsRhoAlgo(n: number): number {
+  let x = 2;
+  let y = 2;
+  let d = 1;
+
+  const g = (x: number) => (Math.pow(x, 2) + 1) % n;
+
+  while (d === 1) {
+    x = g(x);
+    y = g(g(y));
+    d = gcd(Math.abs(x - y), n);
+  }
+
+  if (d === n) {
+    throw new Error("No factors found");
+  }
+
+  return d;
+}
+
 export function odds(percentage: number): Odds {
   console.log(percentage);
   return {
